8ccd8c04c6c59198f6de801927d4cee3ffbb3224
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-04 16:25:13 -0300

    change message to status

--- src/routes/status.route.ts
+++ src/routes/status.route.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from 'http-status-codes'
 const statusRoute = Router()
 
 statusRoute.get('/', (req: Request, res: Response, next:NextFunction) => {
-    res.json({'message': StatusCodes.OK})
+    res.json({'status': StatusCodes.OK})
 })
 
 export default statusRoute
