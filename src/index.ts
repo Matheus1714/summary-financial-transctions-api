5137186c2b7e1cdd07bb869668decf1c0afdac86
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-04 01:47:45 -0300

    configure vercel api

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(statusRoute)
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+const port = process.env.PORT || 3333
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
