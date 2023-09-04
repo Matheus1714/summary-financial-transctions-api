7efa2433371013e456a7fe052daf2b355b3d0a00
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-04 17:57:37 -0300

    add transactionsRoute in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import statusRoute from "./routes/status.route";
+import transactionsRoute from "./routes/transaction.route";
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(statusRoute);
+app.use(transactionsRoute);
 
 const port = process.env.PORT || 3333;
 
