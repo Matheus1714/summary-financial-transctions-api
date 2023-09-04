c5dce0aba97e5ab8772c3051d54fdcd196779891
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-04 17:58:32 -0300

    remove next status route

--- src/routes/status.route.ts
+++ src/routes/status.route.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
 const statusRoute = Router();
 
-statusRoute.get("/", (req: Request, res: Response, next: NextFunction) => {
+statusRoute.get("/", (req: Request, res: Response) => {
   res.json({ status: StatusCodes.OK });
 });
 
