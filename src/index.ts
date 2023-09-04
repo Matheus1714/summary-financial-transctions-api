b9abdbb6f33adf91fc58d8d6a515e338966b3400
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-04 02:09:07 -0300

    configure route to test deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import statusRoute from './routes/status.route'
+// import statusRoute from './routes/status.route'
 import dotenv from 'dotenv'
+import { NextFunction, Request, Response } from 'express'
 
 const app = express()
 dotenv.config()
@@ -8,7 +9,12 @@ dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(statusRoute)
+// app.use(statusRoute)
+
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    return res.send('Express Typescript on Vercel')
+})
 
 const port = process.env.PORT || 3333
 
