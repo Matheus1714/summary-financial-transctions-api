4398dc4c661cc9b47c9ded871c2af60bc84e2f37
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-05 00:27:33 -0300

    config status with default error

--- src/routes/status.route.ts
+++ src/routes/status.route.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from "http-status-codes";
 const statusRoute = Router();
 
 statusRoute.get("/", (req: Request, res: Response) => {
-  res.json({ status: StatusCodes.OK });
+  res.json({ error: 0, status: StatusCodes.OK });
 });
 
 export default statusRoute;
